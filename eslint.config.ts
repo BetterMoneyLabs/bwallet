86cadb309f13e0b46c5fd605579cdad4b58b9a15
User <guest@example.com>
2025-10-23 15:29:05 +0300

    Configure eslint to ignore unused name which starts with underscore

--- eslint.config.ts
+++ eslint.config.ts
@@ -8,24 +8,30 @@ import pluginVue from 'eslint-plugin-vue'
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
-
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
-  {
-      rules: {
-          "@typescript-eslint/no-explicit-any": "error",
-          "@typescript-eslint/no-unsafe-assignment": "error",
-          "@typescript-eslint/no-unsafe-call": "error",
-          "@typescript-eslint/no-unsafe-member-access": "error",
-          "@typescript-eslint/no-unsafe-return": "error",
-          "@typescript-eslint/explicit-function-return-type": "error",
-          "@typescript-eslint/explicit-module-boundary-types": "error"
+    {
+        name: 'app/files-to-lint',
+        files: ['**/*.{ts,mts,tsx,vue}'],
+    },
+    globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+    pluginVue.configs['flat/essential'],
+    vueTsConfigs.recommended,
+    {
+        rules: {
+            "@typescript-eslint/no-explicit-any": "error",
+            "@typescript-eslint/no-unsafe-assignment": "error",
+            "@typescript-eslint/no-unsafe-call": "error",
+            "@typescript-eslint/no-unsafe-member-access": "error",
+            "@typescript-eslint/no-unsafe-return": "error",
+            "@typescript-eslint/explicit-function-return-type": "error",
+            "@typescript-eslint/explicit-module-boundary-types": "error",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                { 
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    caughtErrorsIgnorePattern: "^_"
+                }
+            ]
         }
-  }
+    }
 )
